15818b663ae757f37a73e0ec78fad594305a8f1d
ANTH0RNY <nyaganthony@gmail.com>
2023-06-08 14:06:14 +0300

    Lets see this

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for task 1"""
+
+
+def top_ten(subreddit):
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
+
+    sub_info = requests.get(f"https://www.reddit.com/r/\
+                            {subreddit}/hot.json?limit=10",
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
