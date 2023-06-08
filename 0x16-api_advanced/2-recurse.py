7475002e42b2da6559381e24afd7df6b63831e85
ANTH0RNY <nyaganthony@gmail.com>
2023-06-08 14:12:15 +0300

    Lets see this

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,8 +7,8 @@ def recurse(subreddit, hot_list=[], count=0, after=None):
     of the subreddit"""
     import requests
 
-    sub_info = requests.get(f"https://www.reddit.com/r/\
-                            {subreddit}/hot.json",
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
+                            .format(subreddit),
                             params={"count": count, "after": after},
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
